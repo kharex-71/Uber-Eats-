967c4566cd5dca47d372d37a313e7da96df4bc11
kharex-71 <kharaishvili50@gmail.com>
2023-05-29 13:22:50 +0400

    I added fonts, splash screen and updated the app component

--- App.js
+++ App.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, {useCallback, useEffect, useState} from "react";
 import "react-native-gesture-handler";
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
 import AppNavigation from "./src/navigation/AppNavigation";
 
+
+
 export default function App() {
-  return <AppNavigation />;
+  // const [appIsReady, setAppIsReady] = useState(false);
+  const [uberFontsLoaded] = useFonts({
+    'bold' : require("./assets/fonts/UberMoveTextBold.otf"),
+    'light' : require("./assets/fonts/UberMoveTextLight.otf"),
+    'medium' : require("./assets/fonts/UberMoveTextMedium.otf"),
+    'regular' : require("./assets/fonts/UberMoveTextRegular.otf")
+  });
+
+  useEffect(() => {
+    async function prepare() {
+      await SplashScreen.preventAutoHideAsync();
+    }
+    prepare();
+  }, []);
+
+  if (!uberFontsLoaded){
+    return undefined;
+  }else{
+    SplashScreen.hideAsync();
+  }
+  return <AppNavigation/>;
 }
